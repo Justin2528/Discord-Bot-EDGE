c785c281b96737f91aedbbbe5fe83631d5b8819c
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-01 16:22:36 +0800

    Create botsite.js

---
+++ commands/botsite.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  message.channel.send("[Bot Website](https://discordbots.org/bot/510122334795989003)")
+
+}
+
+module.exports.help = {
+    name: "botsite"
+}
