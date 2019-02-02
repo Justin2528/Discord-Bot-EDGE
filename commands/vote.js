bae0f71ccf80081b9717cbdb6f0e5de09032fda7
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-02 13:58:33 +0800

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("[Vote for the bot!](https://discordbots.org/bot/510122334795989003/vote)")
+  message.channel.send("(Vote for the bot!)[https://discordbots.org/bot/510122334795989003/vote]")
 
 }
 
