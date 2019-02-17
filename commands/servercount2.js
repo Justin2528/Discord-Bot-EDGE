f3867eb73c9dc5d1bc8c9c12a2cc9c493028bb7d
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-17 23:39:27 +0800

    Update servercount2.js

--- commands/servercount2.js
+++ commands/servercount2.js
@@ -4,12 +4,15 @@ const bot = new Discord.Client();
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMDEyMjMzNDc5NTk4OTAwMyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTUwNDE3NzMwfQ.WSNmkbFu0fn_Bi6ocSW8oThNtCPVL3_M6UaPL8E3X4c', bot);
 
 // Optional events
-dbl.on('posted', () => {
+dbl.on('posted', message => {
   console.log('Server count posted!');
+  message.channel.send("ok")
+  
 })
 
 dbl.on('error', e => {
  console.log(`Oops! ${e}`);
+  
 })
     
 exports.help = {
