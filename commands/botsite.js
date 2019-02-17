6447501026d462825b3bfa4b8b299f39bc0ab688
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-18 00:06:52 +0800

    Update botsite.js

--- commands/botsite.js
+++ commands/botsite.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("(Bot Website)[https://discordbots.org/bot/510122334795989003]")
+  message.channel.send("discordbot.org Website [https://discordbots.org/bot/510122334795989003] / botsfordiscord https://botsfordiscord.com/bot/510122334795989003 ")
 
 }
 
