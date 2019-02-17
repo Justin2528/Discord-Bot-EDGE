d080f63a0d616e66c5ebdd132e03e7cb17159061
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-17 23:43:47 +0800

    Update servercount2.js

--- commands/servercount2.js
+++ commands/servercount2.js
@@ -3,6 +3,8 @@ const DBL = require("dblapi.js");
 const bot = new Discord.Client();
 const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMDEyMjMzNDc5NTk4OTAwMyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTUwNDE3NzMwfQ.WSNmkbFu0fn_Bi6ocSW8oThNtCPVL3_M6UaPL8E3X4c', bot);
 
+exports.run = async(bot, message, args) => {
+
 // Optional events
 dbl.on('posted', message => {
   console.log('Server count posted!');
@@ -14,6 +16,8 @@ dbl.on('error', e => {
  console.log(`Oops! ${e}`);
   
 })
+
+}
     
 exports.help = {
     name: "servercount2"
