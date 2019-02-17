a7bcaa46c7923f7125109d78cff75ae539d73c04
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-17 22:02:44 +0800

    Update servercount.js

--- commands/servercount.js
+++ commands/servercount.js
@@ -5,7 +5,7 @@ exports.run = async (bot, message, args) => {
     snekfetch.post(`https://botsfordiscord.com/api/bot/510122334795989003`)
         .set('Authorization', '3e2ae91a8942bb8e59cb10f582de4342b13ac937b10b7f01843542fee894a0bcc85c95ef16ce718b3834fdcfb3bc35f5fee4b37e555e5c7d2fb7803608b9dc79')
         .send({
-            'count': bot.guilds.size,
+            'server_count': bot.guilds.size,
         })
         .then(console.log('Updated DBG Stats'))
         message.channel.send("yes you did it.s")
