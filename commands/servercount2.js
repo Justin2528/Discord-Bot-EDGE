5effbfcf3f99c966ef67918a871c628db0e70209
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-17 23:33:35 +0800

    Update servercount2.js

--- commands/servercount2.js
+++ commands/servercount2.js
@@ -1,15 +1,16 @@
-const snekfetch = require('snekfetch');
-const prettyLog = require('prettyconsole');
-exports.run = async (bot, message, args) => {
-    console.log('updates status');
-    snekfetch.post(`https://discordbots.com/api/bots/510122334795989003/stats`)
-        .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMDEyMjMzNDc5NTk4OTAwMyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTUwNDEwNzM1fQ.Cno0da3O7jvpNmlD05aD1fwBVSEcX7DXi5O4oKJhYKI')
-        .send({
-            'server_count': bot.guilds.size,
-        })
-        .then(console.log('Updated DBG Stats'))
-        message.channel.send("yes you did it.s")
-}
+const Discord = require("discord.js");
+const DBL = require("dblapi.js");
+const dbl = new DBL('Your discordbots.org token', client);
+
+// Optional events
+dbl.on('posted', () => {
+  console.log('Server count posted!');
+})
+
+dbl.on('error', e => {
+ console.log(`Oops! ${e}`);
+})
+    
 exports.help = {
     name: "servercount2"
 }
