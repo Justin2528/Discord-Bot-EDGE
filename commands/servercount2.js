ccf6174e50ac6ddc4d28bd4177119fd37909dd42
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-17 22:06:02 +0800

    Update servercount2.js

--- commands/servercount2.js
+++ commands/servercount2.js
@@ -1,6 +1,6 @@
 const snekfetch = require('snekfetch');
 const prettyLog = require('prettyconsole');
-exports.run = async (client, message, args, ops) => {
+exports.run = async (bot, message, args) => {
     console.log('updates status');
     snekfetch.post(`https://discordbots.com/api/bots/510122334795989003/stats`)
         .set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMDEyMjMzNDc5NTk4OTAwMyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTUwNDEwNzM1fQ.Cno0da3O7jvpNmlD05aD1fwBVSEcX7DXi5O4oKJhYKI')
