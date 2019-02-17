6ba7ac4940a068fe66242a27b42d7e5e4065d0c8
Justin2528 <38654142+Justin2528@users.noreply.github.com>
2019-02-17 23:36:18 +0800

    Update servercount2.js

--- commands/servercount2.js
+++ commands/servercount2.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const DBL = require("dblapi.js");
-const dbl = new DBL('Your discordbots.org token', client);
+const bot = new Discord.Client();
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUxMDEyMjMzNDc5NTk4OTAwMyIsImJvdCI6dHJ1ZSwiaWF0IjoxNTUwNDE3NzMwfQ.WSNmkbFu0fn_Bi6ocSW8oThNtCPVL3_M6UaPL8E3X4c', bot);
 
 // Optional events
 dbl.on('posted', () => {
